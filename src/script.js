cb566a2e3d5b9f35312d63824a706173b00fb252
Samuel Pereira <samuelpereira.dev@gmail.com>
2024-10-19 00:38:02 -0300

    function-calculadora(maneira_corrigida).

--- src/script.js
+++ src/script.js
@@ -1,10 +1,51 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function calculadora(number1, number2, operador){
-          
+function Calculadora(number1, number2, operador) {
+      let number1 = parseInt
+      let number2 = parseInt
+      
+      operador = prompt('digite o operador para seu cálculo: (+), (-), (*), (/), (e)')
+      let resultado
+      
+     switch (operador) {
 
-}
+        case '+':
+          resultado = number1 + number2
+          console.log ('soma: ' + resultado)
+          break
+
+        case '-':
+          resultado = number1 - number2
+          console.log ('subtração: ' + resultado)
+          break
+        
+        case '*':
+          resultado = number1 * number2
+          console.log ('multiplicação: ' + resultado)
+          break
+        
+        case '/':
+          resultado = number1 / number2
+          console.log ('divisão: ' + resultado)
+          break
+          
+        case 'e':
+          resultado = 1
+          let i = 0
+          
+          for(i; i<number2; i++){
+            resultado *= number1
+          }
+      default :
+        console.log('Operador inválido!')
+      }
+      
+      if(resultado === undefined || resultado >=1.0000){
+            resulado === "ERRO"
+            console.log(resultado)      
+      }
+ }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
